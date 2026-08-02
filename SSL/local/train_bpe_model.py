233b5983dd771f591e1aecff5f6525afc2b08f48
AnhNN04 <anhnn281204@gmail.com>
2026-08-03 00:26:30 +0700

    refactor: remove license and copyright headers from python files

--- SSL/local/train_bpe_model.py
+++ SSL/local/train_bpe_model.py
@@ -1,30 +1,3 @@
-#!/usr/bin/env python3
-# Copyright    2021  Xiaomi Corp.        (authors: Fangjun Kuang)
-#
-# See the LICENSE file in the root directory for clarification regarding multiple authors
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
-# You can install sentencepiece via:
-#
-#  pip install sentencepiece
-#
-# Due to an issue reported in
-# https://github.com/google/sentencepiece/pull/642#issuecomment-857972030
-#
-# Please install a version >=0.1.96
-
 import argparse
 import shutil
 from pathlib import Path
